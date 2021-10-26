154bb48e0925098577293ed0116d4079667efd58
Elena <freshjs.ftm@gmail.com>
2021-10-26 10:50:47 +0300

    Add structure slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
+'use strict';
+//очень широкая, очень узкая, квадратная, альбомная, книжная
+const imagesDB = [
+  'https://www.bookbell.in/wp-content/uploads/2018/08/sea-01.jpg',
+  'https://compass-ssl.xboxlive.com/assets/fd/df/fddfac17-875c-4910-ab1f-3f18dd9962b3.jpg?n=Parallax_Sections_Mobile_01.jpg',
+  'https://media-cdn.tripadvisor.com/media/photo-s/14/64/88/c2/makanda-by-the-sea.jpg',
+  'https://bangkokpost.com/photos_content/large/prefix_1/1875/43875/43875-1448252967zcl8ij9iv3.jpg',
+  'https://previews.123rf.com/images/muha/muha1205/muha120500089/13658592-tropical-beach-andaman-sea-thailand.jpg'
+];
 
-
-function logMessage(){
-  console.log('hi!')
-}
-
-const [button] = document.getElementsByTagName('button');
-
-button.addEventListener('click', logMessage);
+const image = document.querySelector('.slide>img');
+const [prevBtn, nextBtn] = document.querySelectorAll('.slider-container>button');
