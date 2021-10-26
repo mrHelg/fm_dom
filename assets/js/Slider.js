1cdf50a3b746678dc4ea96d9da4c01085dccb47a
Elena <freshjs.ftm@gmail.com>
2021-10-26 11:04:33 +0300

    Add class Slider

---
+++ assets/js/Slider.js
@@ -0,0 +1,27 @@
+class Slider{
+  constructor(images=[], currentIndex=0){
+    this._images = images;
+    this.currentIndex = currentIndex;
+  }
+  get images(){
+    return this._images;
+  }
+  get currentIndex(){
+    return this._currentIndex;
+  }
+  set currentIndex(v){
+    if(typeof v !== 'number' || Number.isInteger(v) || v<0){
+      throw new TypeError();
+    }
+    this._currentIndex = v;
+  }
+  get currentSlide(){
+    return this._images[this.currentIndex];
+  }
+  next(){
+    return this._currentIndex+1;
+  }
+  prev(){
+    return this._currentIndex-1;
+  }
+}
