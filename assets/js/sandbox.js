d711a61679417e5df2ca820d51098aa23e5a511d
Elena <freshjs.ftm@gmail.com>
2021-10-27 10:02:46 +0300

    Add Adder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -3,11 +3,22 @@ let i=100;
 
 function createCounter(initial=0){
   let i=initial;//замкнутная переменная
-  return function counter(){
-    return ++i;
+  return {
+    increment(){
+      return ++i;
+    },
+    decrement(){
+      return --i;
+    }
   }
 }
 
 const counter1 = createCounter(10);
-console.log(counter1());
+console.log(counter1.increment());
+console.log(counter1.decrement());
 const counter2 = createCounter();
+
+
+const createAdder = (n) => (m) =>(n += m);
+const adder = createAdder(10);
+
