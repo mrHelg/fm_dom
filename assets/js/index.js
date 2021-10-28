95d7c8dcf184f03e68f7d338562a93537cd88b87
Elena <freshjs.ftm@gmail.com>
2021-10-28 09:58:37 +0300

    Add switchInnerText

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,29 @@
 'use strict';
-const imagesDB = [
-  'https://www.bookbell.in/wp-content/uploads/2018/08/sea-01.jpg',
-  'https://compass-ssl.xboxlive.com/assets/fd/df/fddfac17-875c-4910-ab1f-3f18dd9962b3.jpg?n=Parallax_Sections_Mobile_01.jpg',
-  'https://media-cdn.tripadvisor.com/media/photo-s/14/64/88/c2/makanda-by-the-sea.jpg',
-  'https://bangkokpost.com/photos_content/large/prefix_1/1875/43875/43875-1448252967zcl8ij9iv3.jpg',
-  'https://previews.123rf.com/images/muha/muha1205/muha120500089/13658592-tropical-beach-andaman-sea-thailand.jpg'
-];
-const slider = new Slider(imagesDB);
 
-const image = document.querySelector('.slide>img');
-const [prevBtn, nextBtn] = document.querySelectorAll('.slider-container>button');
+const [openBtn, closeBtn] = document.querySelectorAll('button');
 
-function updateView(){
-  image.setAttribute('src', slider.currentSlide);
-}
-updateView();
+/*поменять местами содeржимое двух по наведению на любую */
 
-const bthSliderHandler = (direction = 'next') => () =>{
-  slider.currentIndex = slider[direction === 'next' ? 'nextIndex' : 'prevIndex'];
-  updateView();
+const switchInnerText = ({target})=>{
+  const openElem = target===closeBtn ? openBtn : closeBtn;
+  const closeElem = target===closeBtn ? closeBtn : openBtn;
+
+  const buffer = openElem.textContent;
+  openElem.textContent = closeElem.textContent;
+  closeElem.textContent = buffer;
+
+  closeElem.removeEventListener('mouseenter',switchInnerText);
+  openElem.addEventListener('mouseenter',switchInnerText);
 }
 
-nextBtn.addEventListener('click', bthSliderHandler('next'));
-prevBtn.addEventListener('click', bthSliderHandler('prev'));
+const clickHandler = ({target})=>{
+  console.log(target)
+}
 
-image.addEventListener('wheel', (e)=>{
-  // const handler = e.deltaY>0? bthSliderHandler('next'):bthSliderHandler('prev');
-  // handler();
-  e.deltaY>0 ? bthSliderHandler('next')():bthSliderHandler('prev')();
-})
+//openBtn.addEventListener('mouseenter',switchInnerText, {once:true});
+closeBtn.addEventListener('mouseenter',switchInnerText);
 
-
-
-/*------------------------------------------- */
-
-// const uniqueImg = document.getElementById('unique');
-
-// const srcAttr = document.createAttribute('src');
-// srcAttr.value = imagesDB[0];
-
-// uniqueImg.setAttributeNode(srcAttr);
-
-/*
-
-По клику на кнопку показывать чередоваинем одну из двух картинок
-
-*/
-
+openBtn.addEventListener('click', clickHandler);
+closeBtn.addEventListener('click', clickHandler);
 
 
