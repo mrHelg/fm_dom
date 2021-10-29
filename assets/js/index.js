0b0a00b4db83ec114dd26e1a8802ffcb08a742c2
Elena <freshjs.ftm@gmail.com>
2021-10-29 11:34:20 +0300

    Add stringToColour & handleImageError

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,23 +11,49 @@ function createActorCards(actor){
   const container = document.createElement('article');
   container.classList.add('cardContainer');
 
+  const imgWrapper = document.createElement('div');
+  imgWrapper.classList.add('cardImageWrapper');
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(actor.name[0]));
+
   const img = document.createElement('img');
   img.classList.add('cardImage');
   img.setAttribute('src', actor.photo);
   img.setAttribute('alt', actor.name);
+  img.addEventListener('error', handleImageError);
+
+  imgWrapper.append(initials, img);
 
   const name = document.createElement('h2');
   name.classList.add('cardName');
-  //name.textContent = actor.name;
   name.append(document.createTextNode(actor.name));
 
   const description = document.createElement('p');
   description.classList.add('cardDescription');
   description.append(document.createTextNode(actor.birthdate));
 
-  container.append(img, name, description);
+  container.append(imgWrapper, name, description);
   card.append(container);
   return card;
 }
+cardsContainer.append(...HTMLElements);
 
-//cardsContainer.append(...HTMLElements);
+/*  handles */
+function handleImageError({target}){
+  target.remove();
+}
+/*  */
+var stringToColour = function(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
