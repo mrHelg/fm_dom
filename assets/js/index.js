36d3fdd62c4bbfae65522e3f311c3bfe3399c9a8
Олег <osumsky@gmail.com>
2021-11-03 11:07:03 +0200

    Add delete func

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,44 @@
 'use strict';
 
-const messageArray = [];
-
+const state = [];
 const form = document.getElementById('root-form');
+const list = document.getElementById('list');
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const {target: { textInput }} = e;
+  const {
+    target: { textInput },
+  } = e;
   const inputValue = textInput.value.trim();
-  //const validator = /(^[A-Z][a-z]{2,12} [A-Z][a-z]{1,17}$)|(^[А-Я][а-я]{2,12} [А-Я][а-я]{1,17}$)/;
-  //const validator = /^[А-Я][а-я]{1,17} ([А-Я]\.){2}$/;
-  
-  if(validator.test(inputValue)){
-    messageArray.push(inputValue);
+  const validatorName = /^[A-Z][a-z]{2,12}$/;
+
+  if (validatorName.test(inputValue) && !state.includes(inputValue)) {
+    state.push(inputValue);
     form.reset();
+    const li = createElement('li', {dataValue: inputValue}, document.createTextNode(inputValue));
+    const btn = createElement('button', {typeEvent: 'click', onClick: deleteLi.bind(li)}, document.createTextNode('X'));
+    li.append(btn);
+    list.append(li);
   }
-  //рендерить на странице то, что добавилось в массив
-  console.dir(messageArray);
-})
 
+});
 
-function createElement(type,{classNames, typeEvent, onClick}, ...children){
+function deleteLi(e){
+  // console.log(this.dataset.value);
+  state.splice(state.indexOf(this.dataset.value), 1);
+  this.remove();
+}
+
+function createElement(type, { dataValue = '', classNames = [], typeEvent = '', onClick = null }, 
+  ...children){
   const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.addEventListener(typeEvent, onClick);//for example
+  if (dataValue) {
+    elem.dataset.value = dataValue;
+  }
+  if (classNames.length) {
+    elem.classList.add(...classNames);
+  }
+  elem.addEventListener(typeEvent, onClick); //for example
   elem.append(...children);
   return elem;
 }
